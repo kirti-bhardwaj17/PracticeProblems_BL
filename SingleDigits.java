a7a23b87b854cfd68a765330972ea98efbf057de
kirti-bhardwaj17 <bhardwajkirti17@gmail.com>
2025-01-18 14:56:46 +0530

    Initial commit

---
+++ SingleDigits.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Digits{
+	public static void main(String args[]){
+	System.out.println("Enter a single digit number");
+	Scanner sc= new Scanner(System.in);
+	int n=sc.nextInt();
+	if(n==1)
+	System.out.print("One");
+	 if(n==2)
+        System.out.print("Two");
+	 if(n==3)
+        System.out.print("Three");
+	 if(n==4)
+        System.out.print("Four");
+	 if(n==5)
+        System.out.print("Five");
+	 if(n==6)
+        System.out.print("Six");
+	 if(n==7)
+        System.out.print("Seven");
+	 if(n==9)
+        System.out.print("Nine");
+	 if(n==8)
+        System.out.print("Eight");
+}
+}
+
